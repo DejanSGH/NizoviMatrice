97cd38554c551b0c7cc77c9f78dd64dc1d329edf
DejanSGH <120726875+DejanSGH@users.noreply.github.com>
2023-01-04 18:56:07 +0100

    3

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,11 @@ public class Main {
 
         int[][] matrica = new int[n][n];
 
+        ucitajMatricu(matrica);
+        ispisMatrice(matrica);
+        prerasporedjivanje(matrica);
+        ispisMatrice(matrica);
+
 
 
     }
@@ -28,7 +33,60 @@ public class Main {
                 matrica[i][k] = scanner.nextInt();
             }
         }
+        scanner.close();
     }
+
+    public static void ispisMatrice(int[][] matrica){
+        for (int i = 0; i < matrica.length; i++) {
+            System.out.println(Arrays.toString(matrica[i]));
+        }
+    }
+
+    public static void swapKolona(int[][] matrica, int prvaKolona, int drugaKolona){
+        int trenutna = 0;
+        int brojac = 0;
+        while(brojac < matrica.length){
+
+            trenutna = matrica[brojac][prvaKolona];
+            matrica[brojac][prvaKolona] = matrica[brojac][drugaKolona];
+            matrica[brojac][drugaKolona] = trenutna;
+            brojac++;
+
+        }
+    }
+
+    public static void swapVrsta(int[][] matrica, int prvaVrsta, int drugaVrsta){
+        int trenutna = 0;
+        int brojac = 0;
+        while(brojac < matrica.length){
+
+            trenutna = matrica[prvaVrsta][brojac];
+            matrica[prvaVrsta][brojac] = matrica[drugaVrsta][brojac];
+            matrica[drugaVrsta][brojac] = trenutna;
+            brojac++;
+
+        }
+    }
+
+    public static void prerasporedjivanje(int[][] matrica){
+        int trenutni = 0;
+        int najmanji = Integer.MAX_VALUE;
+        int indeksNajmanjeg = 0;
+
+        for (int i = 0; i < matrica.length; i++) {
+            trenutni = 0;
+            for (int k = 0; k < matrica[i].length; k++) {
+                trenutni += matrica[k][i];
+            }
+            if(trenutni < najmanji){
+                najmanji = trenutni;
+                indeksNajmanjeg = i;
+            }
+
+        }
+        swapKolona(matrica, 0, indeksNajmanjeg);
+    }
+
 
     public static int najveciBroj (int[] niz){
         int x = niz[0];
@@ -62,16 +120,7 @@ public class Main {
             niz[i] = niz[niz.length-i-1];
             niz[niz.length-i-1] = trenutni;
         }
-    }
-    //NIZOVI
-    //niz[3][3] x 2
-    // [[1,2,3],
-    // [1,2,3],
-    // [1,2,3]]
 
-//    public static void ispisiNiz(int[] niz){
-//        for (int i = 0; i < niz.length; i++) {
-//            System.out.print("," + niz[i]);
-//        }
-//    }
+    }
+
 }
